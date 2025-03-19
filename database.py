4b0f9e9a917443c68c1e4e997e4031261ee0fd64
pokonti <pokonti@bk.ru>
2025-03-19 17:38:45 +0500

    preparing for deployment

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'postgresql://postgres:LZXbnnQOCNTbAvBvvPhNuTIyWISecTYx@postgres.railway.internal:5432/railway'
+URL_DATABASE = 'postgresql://postgres:LZXbnnQOCNTbAvBvvPhNuTIyWISecTYx@interchange.proxy.rlwy.net:26850/railway'
 
 engine = create_engine(URL_DATABASE)
 
