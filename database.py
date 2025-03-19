b987894265e6340c17e78c2d307351994b4741a0
pokonti <pokonti@bk.ru>
2025-03-19 17:31:02 +0500

    preparing for deployment

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'postgresql://postgres:seidazym@localhost:5433/AIQuizLearningPlatform'
+URL_DATABASE = 'postgresql://postgres:LZXbnnQOCNTbAvBvvPhNuTIyWISecTYx@postgres.railway.internal:5432/railway'
 
 engine = create_engine(URL_DATABASE)
 
