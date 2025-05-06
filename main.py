7f50473b923df070b77ce26933b4a054ba672dde
pokonti <pokonti@bk.ru>
2025-05-06 11:56:38 +0500

    auth

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from routes.courses import router as courses_router
 from routes.auth import router as auth_router
 from routes.lessons import router as lessons_router
 from routes.quizzes import router as quizzes_router
+from routes import auth
 
 app = FastAPI()
 # models.Base.metadata.drop_all(bind=engine)
@@ -14,7 +15,7 @@ models.Base.metadata.create_all(bind=engine)
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-app.include_router(auth_router)
+app.include_router(auth.router)
 app.include_router(courses_router)
 app.include_router(lessons_router)
 app.include_router(quizzes_router)
